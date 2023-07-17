8ed591f36d9b5722c7a29ff4e5c7387d9ca7967d
조원주 <dnjswn9178@hanmail.net>
2023-07-17 14:44:38 +0900

    design(members): change dark mode to light and add box shadow

--- front-end/src/librarys/api/members.ts
+++ front-end/src/librarys/api/members.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa6838575cf2ec38bd374af6101a6284d8bd62ce4a5d28a3ad51345db1aae486
-size 236
+oid sha256:26a410e58aebc6a56b418cb93aafaf1f6ca9c4ed1fe82c95e04ec37dbda96cc4
+size 424
