c6188e69b7e28e94ac6129b09941e2c63e24561f
김동훈 <hunn000@gmail.com>
2023-07-17 12:59:05 +0900

    feat(controller): add KakaoController
    Former-commit-id: b294208608adf0260fa035ee0c81a21da4d55acb

---
+++ back-end/Dokkaebi/src/main/java/com/dokkaebi/controller/KakaoController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f1d17a5a31664c3c2443fde26207f3aba899d319c1cc02b038c6390158fe2919
+size 68
--- front-end/src/components/Loading.tsx
+++ front-end/src/components/Loading.tsx
@@ -1,3 +1,10 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:21de62c914b60dc40253097dbda017695e69358edf2a3ce9d45a08e40513d7c0
-size 337
+import React from 'react';
+import { ProgressSpinner } from 'primereact/progressspinner';
+
+export default function CustomDemo() {
+    return (
+        <div className="card">
+            <ProgressSpinner style={{width: '50px', height: '50px'}} strokeWidth="6" fill="var(--surface-ground)" animationDuration=".5s" />
+        </div>
+    );
+}
