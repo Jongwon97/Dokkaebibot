fa6d6c737e9fe8410fe4cb62083d721e26239790
김동훈 <hunn000@gmail.com>
2023-07-17 14:49:29 +0900

    feat: add sklearn files
    Former-commit-id: f53d9fe17d75adf5c68aa4ed94ca25de546715f9

---
+++ iot/dokkaebi/camera/pose/learning.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7307e4c472388c64290d9db4a5042e3b1bd9bccc7a5001bf5fc7147f7f22b823
+size 797
---
+++ iot/dokkaebi/camera/pose/preprocess.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d30519741183c6f32b28fd8a08c3443707993dd5feb34449f43d43f359511fe3
+size 973
