f459d646740a274750cdaa81a7e278ac4c7679cc
조원주 <dnjswn9178@hanmail.net>
2023-07-18 13:22:04 +0900

    fix(members): fix snail to camelCase
    Former-commit-id: d96c9f74c7ec752ac11cf85dec309dfa5d84abd7

--- front-end/src/pages/members/LogIn.tsx
+++ front-end/src/pages/members/LogIn.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0bf1e5f1e9cede17f8498a5e603ef5bf858f2a1e0621e859a353c898cb80b502
-size 2426
+oid sha256:113aae51be7508de77fcfc1532712e180210eb17d56367b21d8074308bf07e99
+size 2686
--- front-end/src/pages/members/SignUp.tsx
+++ front-end/src/pages/members/SignUp.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fba79f3c84d416ccc96f56b73182d24ea0c0a91c4a94bb9d4c3b83088d58c42
-size 2812
+oid sha256:9fe7203d6300bb081b4a64a22fe424b84130f7810d60a7ea845af6f1e2727b1d
+size 2803
