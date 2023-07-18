53de11a3f5b01e66956d04a182a358af634d0bd9
조원주 <dnjswn9178@hanmail.net>
2023-07-18 13:43:49 +0900

    design(members): sns button layout
    Former-commit-id: 3fe43c3257b82a951bc0f5a0ab7d840bd7c38fba

--- front-end/src/pages/members/LogIn.tsx
+++ front-end/src/pages/members/LogIn.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:113aae51be7508de77fcfc1532712e180210eb17d56367b21d8074308bf07e99
-size 2686
+oid sha256:3d8d05c695181c0c5e44b29134b139c080ff28343487c565cee5cb0c0ed6f76a
+size 2850
--- front-end/src/pages/members/SignUp.tsx
+++ front-end/src/pages/members/SignUp.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9fe7203d6300bb081b4a64a22fe424b84130f7810d60a7ea845af6f1e2727b1d
-size 2803
+oid sha256:0baed25fb44ddd7fefbb4647fadec971c04b03ae839ac91e08dd5269c48b5a3b
+size 3950
