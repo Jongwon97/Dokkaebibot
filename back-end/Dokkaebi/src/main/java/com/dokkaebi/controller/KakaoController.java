604fed454d51659d6c849bcb0ddb18602a0de163
김동훈 <hunn000@gmail.com>
2023-07-18 17:05:06 +0900

    feat: add redirect to bridge page
    Former-commit-id: 870f841dc1d60e13c17bc84515516bc11dfdb47c

--- back-end/Dokkaebi/src/main/java/com/dokkaebi/controller/KakaoController.java
+++ back-end/Dokkaebi/src/main/java/com/dokkaebi/controller/KakaoController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35c46340666c325cac534b29d1ae85fb9cd718c015711dc82cc8ca2d1196be5a
-size 1249
+oid sha256:3474dd4d54a828931ba45f75f56d07f6c24c2cbb43ddd785ea2118c7c8a8f83d
+size 1823
