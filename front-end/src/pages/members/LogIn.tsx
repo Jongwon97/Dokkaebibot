558dcc6dec0f567e214009b37a22a4d42d8b6f34
김동훈 <hunn000@gmail.com>
2023-07-18 12:46:31 +0900

    fix(pages): delete unused variables
    Former-commit-id: 54855bf74c5e7fa46861bd0238963ae61b48725f

--- front-end/src/pages/members/LogIn.tsx
+++ front-end/src/pages/members/LogIn.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e3059eaf512ab87a9fe77c31f6d6e2776145817e8c512cea75de7c717f3dad4
-size 2461
+oid sha256:0bf1e5f1e9cede17f8498a5e603ef5bf858f2a1e0621e859a353c898cb80b502
+size 2426
