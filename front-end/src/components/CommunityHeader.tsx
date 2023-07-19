90e8de3985cd0d02233fde701b1b3da9326dde9c
조원주 <dnjswn9178@hanmail.net>
2023-07-19 14:52:28 +0900

    feat(community): split community header components
    Former-commit-id: 51d0bfcd50693a06c49b7b0d24da03019ac799c9

---
+++ front-end/src/components/CommunityHeader.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e366bbe88172dd0992643cd5212d7a7da1608be0bb695f3c2de3005a516e88d4
+size 466
--- front-end/src/pages/community/Community.tsx
+++ front-end/src/pages/community/Community.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db235654f8cf61950c0bb84330952d1b9a414fd00d2ff145c6f5e8921cb2e03d
-size 1534
+oid sha256:131694eba5e543b0b06384c5d3c9d5f05ff1901e4b78040d45b1591515e091dd
+size 1029
