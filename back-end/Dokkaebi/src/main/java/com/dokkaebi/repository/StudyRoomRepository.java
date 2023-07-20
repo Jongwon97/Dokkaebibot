7d33d810770bbeaad38435263791301178f3bc53
이종원 <whddnjs3214@naver.com>
2023-07-20 17:57:10 +0900

    StudyRoom create
    Former-commit-id: f32d749aa3e44f567c4b973a10645f192332cd4f

--- back-end/Dokkaebi/src/main/java/com/dokkaebi/repository/StudyRoomRepository.java
+++ back-end/Dokkaebi/src/main/java/com/dokkaebi/repository/StudyRoomRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49223211de4b4e90012564c5de0bd0d916df044e91e01592e799ed6cfb2dba97
-size 289
+oid sha256:973d860a074dd2c1bd96aa9d5152377e72c11bd65f8bb3595a3e00dbbc000740
+size 477
--- back-end/Dokkaebi/src/main/java/com/dokkaebi/service/studyroom/StudyRoomService.java
+++ back-end/Dokkaebi/src/main/java/com/dokkaebi/service/studyroom/StudyRoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1bc8397ad64f176a7edc60b45b34bf17c5aa487855c44cbee73aa136afad691a
-size 232
+oid sha256:7f027a2e9be819e2bd9a50f7302af31559df9380e3a51d338f123f2960f7fd85
+size 390
