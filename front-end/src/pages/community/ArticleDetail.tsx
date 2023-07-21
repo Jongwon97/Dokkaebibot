b61b21f16c0595f312d771fe555bc6cf3965a5de
조원주 <dnjswn9178@hanmail.net>
2023-07-21 12:40:37 +0900

    feat(community): add Article Detail contents and like scrap buttons
    Former-commit-id: 0620ca3ea1d46cb9578bbe9134a3ba096f36dfbf

--- front-end/src/pages/community/ArticleDetail.tsx
+++ front-end/src/pages/community/ArticleDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:392e7afd1e89ab29bd6409d19991bba30a6093eddfc799d8e988ec52a4b577e4
-size 2289
+oid sha256:a209c495a605abac02560637de981273cfd7600ecde9dcca00908ece5748d41b
+size 2511
