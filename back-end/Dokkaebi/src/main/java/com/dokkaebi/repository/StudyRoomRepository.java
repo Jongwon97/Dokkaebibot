12159e8010ed7c15a8119463bebce0d539d896d4
이종원 <whddnjs3214@naver.com>
2023-07-21 11:06:21 +0900

    login interceptor
    Former-commit-id: 920a341b0a8ec45e760332db843c19efcbe883f8

--- back-end/Dokkaebi/src/main/java/com/dokkaebi/repository/StudyRoomRepository.java
+++ back-end/Dokkaebi/src/main/java/com/dokkaebi/repository/StudyRoomRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:973d860a074dd2c1bd96aa9d5152377e72c11bd65f8bb3595a3e00dbbc000740
-size 477
+oid sha256:6e02015a535803e3a58450a273f36d97e867737d29e82c588f6427cedac6fbdc
+size 486
