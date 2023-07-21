ed37a4e99d042c667f7e0ddb284421e2b4d3d8f5
조원주 <dnjswn9178@hanmail.net>
2023-07-21 15:10:29 +0900

    feat(community): add Review components and type reducer
    Former-commit-id: fbb6108fdcfa1e9247cdf3c20617e939781c0c7c

--- front-end/src/pages/community/ArticleDetail.tsx
+++ front-end/src/pages/community/ArticleDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63e7c9f3c888bf5398f37cab238df1cbac20b161c0a3eef57b0a6a8c930f78f9
-size 3154
+oid sha256:bdbc65e05bc541251461d407ddb639ab53cc3fef3cd7e92aeac7e46f5e7122be
+size 3239
