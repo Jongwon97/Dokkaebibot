d2e0fcc291b28b23c756415a94ffd842a4fc8513
김동훈 <hunn000@gmail.com>
2023-07-21 12:59:24 +0900

    fix: change post article return value
    Former-commit-id: 85aa7a859e76d809dd21d7d663032d9efe1ecf61

--- front-end/src/pages/community/comp/ArticleEditor.tsx
+++ front-end/src/pages/community/comp/ArticleEditor.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1fbf0e6f7356eb24ebfd46fd974cfdc2b0d8ba36cf2c119734a2972c8f3568c4
-size 2358
+oid sha256:f281bc246a154c31e25275def23d15141f6d4f22c8e9926bbfbae94ebc1f7bb7
+size 2595
