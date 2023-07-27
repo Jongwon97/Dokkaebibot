a5b0bfaf653306a759b8efde6ee89779f1df47a7
조원주 <dnjswn9178@hanmail.net>
2023-07-27 13:01:09 +0900

    feat(members): mypage profile add
    Former-commit-id: bcc36d706061c37dacf073ebd97e18e12c644066

--- front-end/src/pages/members/MyPage.tsx
+++ front-end/src/pages/members/MyPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea7fb479479399f104f7cf8163aa6de2ba5bcd8b595f9c7a4bc5c33ef53fff62
-size 1281
+oid sha256:71a094d8a4b884053ec0edd1250ac2a492cae244a374b3e5bf337009f760569b
+size 1704
