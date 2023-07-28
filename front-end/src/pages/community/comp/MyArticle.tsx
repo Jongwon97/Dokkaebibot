73f54f467d50fbaba286aecb2d553183d123c14e
조원주 <dnjswn9178@hanmail.net>
2023-07-28 15:49:10 +0900

    fix(community): myarticle edit
    Former-commit-id: 183bfb395f2a6727263729caffdbede1c09e4fc7

--- front-end/src/pages/community/comp/MyArticle.tsx
+++ front-end/src/pages/community/comp/MyArticle.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b034d8bbe44f36409167d56b17f635c103a2f5c89c01e7a05f7f3e645b05e908
-size 1442
+oid sha256:f1b7ba0cf52dc53842b2a01b008167170e8a21fe58c5bb6898a3fd11b580e770
+size 1421
