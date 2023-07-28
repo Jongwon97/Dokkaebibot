5daea74cf5bd1cb640ff446be16a189f21619d09
홍의선 <hon3538@gmail.com>
2023-07-28 14:06:44 +0900

    refactor(camera/object) : test
    Former-commit-id: 4e0674d831fbc9aa30542e3a35b290e651555946

--- iot/dokkaebi/camera/test3_for_vscode.py
+++ iot/dokkaebi/camera/test3_for_vscode.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b9e1b723f9103b3c6623a3d17b73276ce2064e6143406ec7b838e4cc722de8e
-size 5464
+oid sha256:0234ab35dcf62bea198d67ef838a056152359d131ea37293a850d18bd1b4b449
+size 5470
