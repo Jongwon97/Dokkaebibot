9af1fc1902a3e168b067d842c71a8c01d26f1ad4
이종원 <whddnjs3214@naver.com>
2023-07-31 14:54:49 +0900

    image s3 upload
    Former-commit-id: 72ba74d9c8a34dd326a3a10b4823342424457696

--- back-end/Dokkaebi/src/main/java/com/dokkaebi/service/studyroom/StudyRoomService.java
+++ back-end/Dokkaebi/src/main/java/com/dokkaebi/service/studyroom/StudyRoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33dc0d62c33e1a16f554728d9e8093df783d8b6164abe56e0b66ec1870e6ef3a
-size 444
+oid sha256:17e6c2be6bbad07d427befa486750036956311e8f67f0dc89da08c177848c9e2
+size 505
