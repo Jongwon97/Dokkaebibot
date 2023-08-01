5e914c0dc35f01141b4956a4d1a366d45faba729
조원주 <dnjswn9178@hanmail.net>
2023-08-01 10:49:37 +0900

    feat(studyroom): use studyroom reducer instead of useState
    Former-commit-id: a1186729362a8d9de549cc4e151ad27aaa6bf96e

--- front-end/src/pages/community/StudyroomCreate.tsx
+++ front-end/src/pages/community/StudyroomCreate.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f88f656d11ac33e5aa40a0ca0492349fdc08456c826768b71f6167e88ac0a9c4
-size 5365
+oid sha256:2040c564bc8dedb4447802354da779fb8be458f117748daf7735e30fb42cb2cd
+size 5454
--- front-end/src/redux/reducers/studyroomReducer.ts
+++ front-end/src/redux/reducers/studyroomReducer.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e09c5680ee8de395ccc64e661ceab8c82b0d96ce9835885c466ea303b0db9d5c
-size 1216
+oid sha256:3e1045adbd77dcde57fe703fb282253ba0d9d53cb23ebd0f1f352e22e8b51623
+size 1051
