f4e041a36d8edee2985f6feab9192f603adb6d19
조원주 <dnjswn9178@hanmail.net>
2023-08-01 16:16:23 +0900

    feat(alert): add fetchAlertList useEffect
    Former-commit-id: 6476160a5530c689cceb9f9b987430f9ad7d853d

--- front-end/src/components/Alert.tsx
+++ front-end/src/components/Alert.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7cbbff4b9989dcb43ef11030e7b48ed8b58fabac93412abe537339ff55212f90
-size 1104
+oid sha256:464c92685098096d48f579d18b79248bf205ee93de13608b7d2c0b160beecf4b
+size 1395
