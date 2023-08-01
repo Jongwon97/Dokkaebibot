c44d648d22bcfc3adf83ba5d2dba7dc0a1fb7eac
김동훈 <hunn000@gmail.com>
2023-08-01 15:15:59 +0900

    Update back-end/Dokkaebi/src/main/java/com/dokkaebi/config/CorsConfig.java
    Former-commit-id: 708e962b84bf0839df1f6cdab603b8d40978248c

--- back-end/Dokkaebi/src/main/java/com/dokkaebi/config/CorsConfig.java
+++ back-end/Dokkaebi/src/main/java/com/dokkaebi/config/CorsConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29ed9e5072b04ff1a5ec9cb6e56f21f18327f2fba045a6c11a78e80bba260c56
-size 548
+oid sha256:f5d1bf2bdbe5cf28ec4d31886bff98f34c9a85560069b0dd22ad6ee5cedf0ef2
+size 654
