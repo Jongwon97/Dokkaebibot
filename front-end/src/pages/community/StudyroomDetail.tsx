d8e32c8123dea6bda6715e10a811c12f138a1b9f
조원주 <dnjswn9178@hanmail.net>
2023-08-01 10:50:10 +0900

    feat(studyroom): detail page header img change
    Former-commit-id: 6cca223ff1e30cb9e5108da02db7c4b9801ae7dc

--- front-end/src/pages/community/StudyroomDetail.tsx
+++ front-end/src/pages/community/StudyroomDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7ea1c677479f0051c219bfccfacc81407e6f581ed816fd4ca3c7d3c10c6fa0e
-size 1182
+oid sha256:22129f8a9fe8e3c00d494f79bf2188c72a932cb9d11aadd549767b3349c4a72b
+size 1241
--- front-end/src/pages/community/comp/CommunityHeader.tsx
+++ front-end/src/pages/community/comp/CommunityHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac6e2cf30e8354bf4992090d57bff59cdbbb19e8dd081e72b49db93c87fa2402
-size 1374
+oid sha256:378040c836aec605de5614c23d62ac4be832b8bebd35f5f53eb4e91549eff021
+size 1428
