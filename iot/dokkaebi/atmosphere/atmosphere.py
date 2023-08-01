7669d90642ad78a7f99da4e05634b7c75be69894
42.4.hyojlee <42.4.hyojlee@gmail.com>
2023-08-01 15:51:44 +0900

    feature(atmosphere/dht11): Read temp & humid
    Former-commit-id: bf1cd9342e5d19eb76bc6be1a6bfdab0889fd110

---
+++ iot/dokkaebi/atmosphere/atmosphere.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8eda778e613a7de9932186eb63d6bd573d6f090b7451300c8da9719ebdec31eb
+size 374
---
+++ iot/dokkaebi/atmosphere/dht11.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4a3338293750248f4bd7e21448cc2d12fad2152963b3e67ad88ccaf84ffe1f80
+size 437
