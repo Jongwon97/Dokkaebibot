279ef06d3b2f66350070c2f0736352780eb0680e
조원주 <dnjswn9178@hanmail.net>
2023-08-01 15:26:06 +0900

    feat(studyroom): add exit alert confirm dialog with styles
    Former-commit-id: 4a1641e641ec9add2b3f76534befc893d2e5ee9e

--- front-end/src/pages/community/StudyroomDetail.tsx
+++ front-end/src/pages/community/StudyroomDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5761012f7a39f78268f9dfb750f1055a8500a31db4946eb7fd844ce96e3d252
-size 1958
+oid sha256:7d3c9e29629686fd1a011b33dff2591c0e2e112118bbffe9cc06f09be242d1b7
+size 1988
