b0ae77c33d3d60eeb393833ad24f3680c26b8250
김동훈 <hunn000@gmail.com>
2023-08-01 14:48:57 +0900

    Update front-end/src/pages/members/LogIn.tsx
    Former-commit-id: 9f7f1cd35b3b90870beb7ce2baf61a65de68700c

--- front-end/src/pages/members/LogIn.tsx
+++ front-end/src/pages/members/LogIn.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd651063ddacff0c43ed8a5a32740807a0687802a130489b3d46b39fceeb4042
-size 3538
+oid sha256:bf299163c67d475577bb34d29b42ae580306213ba48b4bc014e635b03d7ec69e
+size 3547
