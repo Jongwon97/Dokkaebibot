443e283f620f68d5ccafcc48c01144f8d8032ce3
조원주 <dnjswn9178@hanmail.net>
2023-08-01 14:04:10 +0900

    feat(studyroom): add exit confirm with styles
    Former-commit-id: 5e194deebddd645bed3e24ff8d946c2f7add8dfc

--- front-end/src/pages/community/StudyroomDetail.tsx
+++ front-end/src/pages/community/StudyroomDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22129f8a9fe8e3c00d494f79bf2188c72a932cb9d11aadd549767b3349c4a72b
-size 1241
+oid sha256:f5761012f7a39f78268f9dfb750f1055a8500a31db4946eb7fd844ce96e3d252
+size 1958
