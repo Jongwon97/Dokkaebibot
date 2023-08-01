7478670a3d3c6f63380886fa87ccb51cc5a0d80f
이종원 <whddnjs3214@naver.com>
2023-08-01 15:32:01 +0900

    feature: Studyrooms 조회 분리
    Former-commit-id: 61563cee6f942388bcebd141b5ccc46b41b09f4f

--- back-end/Dokkaebi/src/main/java/com/dokkaebi/controller/StudyRoomController.java
+++ back-end/Dokkaebi/src/main/java/com/dokkaebi/controller/StudyRoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4196067fc54fea8c2183f1687546d5c847fb84fad2bc5b122f17a0e329a14b9b
-size 2794
+oid sha256:d39660a334fad5f77cd07f186b3b3fe28c02e675176e78b6fb01475f924bc0b1
+size 3013
--- front-end/src/pages/community/comp/StudyroomListItem.tsx
+++ front-end/src/pages/community/comp/StudyroomListItem.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f67e955947982d7c5ec028135281910c589aee531850450cb868652e7881179
-size 2437
+oid sha256:fdc3b7ea7195528ebb7260c13aeb06b8d1aeb91b8d3c9c808d048d5909edc16e
+size 2335
