b797c717336ec35199dc3809f0aae0f535f23991
조원주 <dnjswn9178@hanmail.net>
2023-08-01 15:37:16 +0900

    feat(chat): add chat modal components with layout
    Former-commit-id: 22d2b28e0f61c528507af6aab6dbf9e5d2bbce9c

--- front-end/src/components/Chat.tsx
+++ front-end/src/components/Chat.tsx
@@ -1,11 +1,3 @@
-import React from 'react';
-
-const Chat = () => {
-  return (
-    <>
-      chatting room
-    </>
-  );
-};
-
-export default Chat;
+version https://git-lfs.github.com/spec/v1
+oid sha256:851ff4f500567f29e4713e77bca58d8b1ff9b6122e2d38aa8f8a7ac4fd14573c
+size 398
