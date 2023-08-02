5f8402adb069e39fff614871c4569dcda92b37d0
홍의선 <hon3538@gmail.com>
2023-08-02 15:58:38 +0900

    fix(camera/object): class structure
    Former-commit-id: e092baed1b538b7890d1f79c26892734140f79ef

--- iot/dokkaebi/camera/face/Face.py
+++ iot/dokkaebi/camera/face/Face.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae1b1898a28f9f91af6d4ecbb022eaefb95f25eedd5e0d727a3f402140e2769e
-size 5321
+oid sha256:621e9d19d0a2bdd7919d744fe36a473a7fe4a9e57066eaf4c054dd288ef00fea
+size 5553
--- iot/dokkaebi/camera/object/Object.py
+++ iot/dokkaebi/camera/object/Object.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94f5185486b4fc91fab1c9c8f5323d6e5f3c3d55e7bc5cba7520c19e959e1837
-size 1722
+oid sha256:c75dc820a0e2542ca102830178aa34adc78c3b8e777952e69f6fbb2e06c62e5d
+size 3466
