ef1117551d831232e72b5c7865da1723f6f46511
이종원 <whddnjs3214@naver.com>
2023-08-02 09:40:35 +0900

    feat: StudyRoomDetail update
    Former-commit-id: 22f11baad1b5701b8d5e218b9144cf3995be4af3

--- front-end/src/pages/community/comp/CommunityHeader.tsx
+++ front-end/src/pages/community/comp/CommunityHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e148713fa9bba176ebd3995e69eea8539cad787719b0ac33d78e995407264f6
-size 2004
+oid sha256:6c72503fc0f259c23276e224bb9c7ed969d638b1d3ed011fe7631e79ac7884d7
+size 2030
