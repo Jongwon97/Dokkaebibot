56873295ac0c5cb16db4b0c38cd047a33686ec47
이종원 <whddnjs3214@naver.com>
2023-08-02 10:34:27 +0900

    merge
    Former-commit-id: b7027c8d3796cd3b3f384a95d6dd0f88c8716fca

--- front-end/src/client/studyroom.ts
+++ front-end/src/client/studyroom.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:652fde91095c612c8bbb5e14eeb8b03c4ca3c3cfc239732ebd964dd748f7f582
-size 889
+oid sha256:b6e8230e7ad6a65cb24aa0968b86728e3a2889a8d30b599b8d9f027a96a682a2
+size 888
