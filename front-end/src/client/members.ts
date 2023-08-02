98616c255a46d3ff03617a9a68dc7154ff6c1617
김동훈 <hunn000@gmail.com>
2023-08-02 12:12:30 +0900

    fix: client to client()
    Former-commit-id: e85ed6f4ef91dfbf25a1ae3194ef65c864643cf4

--- front-end/src/client/members.ts
+++ front-end/src/client/members.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1ccf0eee8acb562a8c8fc02faa2642eadc1ef3d84bc99f9e35b9e691a020d69
-size 967
+oid sha256:5d2a176a14b540cdf0f85dd4b555b39b1b680897b75d9aae4be8a58a0fb13406
+size 969
