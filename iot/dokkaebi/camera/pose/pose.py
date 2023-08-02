771721ef03ff3aef3e68ee11e3b5bd48f886a053
이효정 <42.4.hyojlee@gmail.com>
2023-08-02 14:10:56 +0900

    fix(camera/pose): pose detection algorithm
    Former-commit-id: 4f310f0e11df406c0425ec5a6e94c2a604040832

--- iot/dokkaebi/camera/pose/pose.py
+++ iot/dokkaebi/camera/pose/pose.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ca2b654246f2f3a0ca61e951e1c93ede607fe2903d7888db9e591b633da7af9
-size 2885
+oid sha256:006db21ad3543080752f13b7b9fb2e47a6ac4f2c755e0cb428ffb01dc1666114
+size 3431
