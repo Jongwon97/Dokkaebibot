a9852d58867d5e3716af68573be8b9058dab6b5e
조원주 <dnjswn9178@hanmail.net>
2023-08-02 15:59:21 +0900

    feat(chat): add chat input field
    Former-commit-id: 1933440da74adf0cbd641ca13c0d25c1b33293b9

--- front-end/src/components/Chat.tsx
+++ front-end/src/components/Chat.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78813998c85fc7940db2a140140b3c4a5785ebc971cd951b588555f5b5a5d540
-size 2328
+oid sha256:1a829799e677e4b04a724fba6771015329c5b767c7ccb2237742e2c8e0b6befc
+size 3216
