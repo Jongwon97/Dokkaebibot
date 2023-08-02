a16459df54a01d422c5b09ed6e7c48fc6ed0ae56
이종원 <whddnjs3214@naver.com>
2023-08-02 09:16:03 +0900

    update
    Former-commit-id: 7fbeca23035289b9b7519a58d1c4f23bfa494fa5

--- front-end/src/pages/community/comp/CommunityHeader.tsx
+++ front-end/src/pages/community/comp/CommunityHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8db5c14a93a77c6395bfc25b69f50d9422240fe47a35538752ad22233ad26af5
-size 1907
+oid sha256:7e148713fa9bba176ebd3995e69eea8539cad787719b0ac33d78e995407264f6
+size 2004
