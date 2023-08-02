6a4a6d6b78563894cf81e8dd30e5f638a78763e6
조원주 <dnjswn9178@hanmail.net>
2023-08-02 14:17:16 +0900

    feat(community): add dompurify to show contents with html tags
    Former-commit-id: 6475ec2ad53298a40a72c9c48c632fb42f51b285

--- front-end/src/pages/community/ArticleDetail.tsx
+++ front-end/src/pages/community/ArticleDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73a9ac37bf598e8e0d982501edd9dc6244292bd35a0af28a684dffe7f2f92fbf
-size 4648
+oid sha256:8bd001dcf5bcf22d66c5d648527aa6ac6a41089e4e7e9011c7982c51045675a9
+size 4845
