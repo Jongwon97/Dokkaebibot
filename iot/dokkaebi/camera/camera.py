c016788778a01ef40f75891b43628f53c8e6552b
이효정 <42.4.hyojlee@gmail.com>
2023-08-03 09:49:01 +0900

    fix(iot/camera): openCV 응답없음
    Former-commit-id: dd8ba2f3142b3813a4c8960d4f8458c7ee9608af

--- iot/dokkaebi/camera/camera.py
+++ iot/dokkaebi/camera/camera.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:526591e4ba78200e2b8186fdab529ec8de73d60363675351ab8a39c941e92d17
-size 4648
+oid sha256:e63093d5eddbdd581f5b54265c0c0cf3f81dfa7df9cb56ae9bb687a6d5d7dffb
+size 4778
