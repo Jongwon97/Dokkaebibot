452be5065a8b9a790ee04ad942fa3f83ef73c390
조원주 <dnjswn9178@hanmail.net>
2023-08-03 15:34:19 +0900

    feat(studyroom): add 1:N chat
    Former-commit-id: fff0ec3556bd32890e4ed89304e6cf9e062b4dee

--- front-end/src/pages/community/StudyroomDetail.tsx
+++ front-end/src/pages/community/StudyroomDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:121e59dffa2f26d3ca3f82f3089a55354b74caf93633f28d0ee6eed42d5b3ec1
-size 3160
+oid sha256:261806b6196b36116ec0c739dbf30e67184a3e7d4e40a3c7f1b3e3750cc5b6e4
+size 3297
---
+++ front-end/src/pages/community/comp/StudyroomChat.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:df8c42dbe1f0f620e7ab4002aea7340cfa3d961cdc04f329170ac7c088196bb6
+size 992
