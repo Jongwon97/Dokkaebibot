7fbcf9383914ff53ab0e99c0988ec771d762e2b5
김동훈 <hunn000@gmail.com>
2023-08-03 14:51:37 +0900

    feat: add new message dot
    Former-commit-id: 6276599e5a7f62b662c214134f9fd8a11915f24b

--- front-end/src/pages/members/comp/FriendSetting.tsx
+++ front-end/src/pages/members/comp/FriendSetting.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a92fcef32ccd161b66a763f26e7d2cacd6524d26d57eb60692fff3ded6a13f5
-size 3742
+oid sha256:11d20e7a7a8b5014ce614f529cefe2b84c7ff5c635212ff551527c14222bab4e
+size 3907
