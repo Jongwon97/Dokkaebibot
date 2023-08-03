6a8ec8cb87cadd06619ac9f88d89a07a9f3faa33
조원주 <dnjswn9178@hanmail.net>
2023-08-03 15:00:20 +0900

    feat(members): add member mypage setting skeltons
    Former-commit-id: 882ebcbf7a8fcdac3d0331dcae83c0ea308720a1

--- front-end/src/pages/members/comp/RobotSetting.tsx
+++ front-end/src/pages/members/comp/RobotSetting.tsx
@@ -1,11 +1,3 @@
-import React from 'react';
-
-function RobotSetting() {
-  return (
-    <div className="RobotSetting">
-      RobotSetting
-    </div>
-  );
-}
-
-export default RobotSetting;
+version https://git-lfs.github.com/spec/v1
+oid sha256:4c5a92b2633aea6ba124a1c57e3d7cfc6e567218f266b15a4fedddd4d78ef4a5
+size 584
--- front-end/src/pages/members/comp/StudySetting.tsx
+++ front-end/src/pages/members/comp/StudySetting.tsx
@@ -1,11 +1,3 @@
-import React from 'react';
-
-function StudySetting() {
-  return (
-    <div className="StudySetting">
-      StudySetting
-    </div>
-  );
-}
-
-export default StudySetting;
+version https://git-lfs.github.com/spec/v1
+oid sha256:88e173cdd1dcb23f50e361cee3a979280945df510ef9442a75e918a67ebedab6
+size 373
