ee0a201396aa7b2cb4137aac1d77e0fc47636c1b
조원주 <dnjswn9178@hanmail.net>
2023-08-03 14:59:23 +0900

    feat(members): add myStudyroomList in friendSetting
    Former-commit-id: b814ae3a16f6cb356cd60691fd8da13d21ae5ff2

--- front-end/src/pages/community/comp/StudyroomListItem.tsx
+++ front-end/src/pages/community/comp/StudyroomListItem.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22c8f3b28a8014f987dfb0e8a647d21b0508f543f7b3e1b127c1e6dd30b9637e
-size 2193
+oid sha256:b1461b14879cd367691a9344360cbdc9fe2035e99917437e455869eee8653a3f
+size 2852
