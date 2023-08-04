0e8d2731c61f74e398cb30ed7a7e8674750ab68a
조원주 <dnjswn9178@hanmail.net>
2023-08-04 13:44:17 +0900

    style(header): delete never used things
    Former-commit-id: 1a5b14b5e54b0e3f5a9f4b843a1fb6ee4dded459

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4746c7a25c2ab7979cb8179b51fbffc8dbb42d48d09b28ade5a07841e0bfbb6
-size 2791
+oid sha256:e25c99bb3744ca2a06e130de04d612fadfaad14432e6140704dc90c4108598ec
+size 2654
