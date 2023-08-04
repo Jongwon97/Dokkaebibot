7c87caf846f7b1734411aff3e85a80e622be07aa
김동훈 <hunn000@gmail.com>
2023-08-04 17:30:29 +0900

    fix: add nickname on articlelist
    Former-commit-id: 5b2764f24aa2a1809b5b73197b1d455da93486af

--- back-end/Dokkaebi/src/main/java/com/dokkaebi/domain/community/ArticleDTO.java
+++ back-end/Dokkaebi/src/main/java/com/dokkaebi/domain/community/ArticleDTO.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:528dfaaf3f684c0d26f635670134af3cc97f15e9af1d9087d0a9e3fcd766b887
-size 854
+oid sha256:e297619ed6f4186707a76d7a6ed42d398c47e3b739213f0a936b92ca5ccb0836
+size 955
