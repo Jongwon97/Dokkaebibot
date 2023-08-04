e5bd412bf15f582589d9d3c9216519a2885a19dd
Byeongmin Ryoo <bmryu0501>
2023-08-04 15:43:25 +0900

    fix(atmosphere.py): return temp/humid even if None
    Former-commit-id: f3b4fb9c90358d146005b8e95cf9db8e950e597a

--- iot/dokkaebi/atmosphere/dht11.py
+++ iot/dokkaebi/atmosphere/dht11.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94062a95265f1b386dc25aa219b2e4b97c6f9e8946df11c4be791dab0bc87888
-size 695
+oid sha256:77ae6de016cb35103ebe82206686e53e0646d00b16f90dc2e1000d028eed3af1
+size 732
