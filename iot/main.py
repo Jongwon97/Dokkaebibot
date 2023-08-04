e085017cf43f9bd5240d0f5b7f31e85ae51139b4
Byeongmin Ryoo <bmryu0501@gmail.com>
2023-08-04 10:32:40 +0900

    feature(main.py): add debug code
    Former-commit-id: e512971af46b7de7f5df1ed9b62a58056e78914a

--- iot/main.py
+++ iot/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa188d2838d9431be34da3e82f5a6878c536909687e4db34b2c693ed7c3ae2fb
-size 2498
+oid sha256:1f231a1c5b6b77a89f558dc1c0f8395e017ba9a8cdf3741d8aa3eaf6f3d4aa1b
+size 2849
