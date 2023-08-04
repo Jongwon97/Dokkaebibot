cd487385a6058e1fd44883cd36792251ac1b91da
이종원 <whddnjs3214@naver.com>
2023-08-04 15:06:14 +0900

    chat stomp complete
    Former-commit-id: 927749c6c0b34001deb62b9edd38ece465db3cab

--- back-end/Dokkaebi/src/main/java/com/dokkaebi/domain/community/MessageRequestDTO.java
+++ back-end/Dokkaebi/src/main/java/com/dokkaebi/domain/community/MessageRequestDTO.java
@@ -1,3 +1,10 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3ad3ac51c188c02b53556de3c241b6ebbf0425e060c017fa94bbfee17dde0ea9
-size 180
+package com.dokkaebi.domain.community;
+
+import lombok.Data;
+
+@Data
+public class MessageRequestDTO {
+  private String content;
+  private Long senderId;
+  private Long receiverId;
+}
