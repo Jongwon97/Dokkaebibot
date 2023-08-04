bb33412a4cbae25f174fe1ba6decde0e819286cd
조원주 <dnjswn9178@hanmail.net>
2023-08-04 13:42:21 +0900

    feat(header): add header sidebar for mobile
    Former-commit-id: d411d0f7ed1fe95eac3f7bb0674ead68f42c46f0

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a252c718999e789c5308c9e667bea2d4abc97f1032cbb9a27a10ccc8d95c160
-size 3217
+oid sha256:d4746c7a25c2ab7979cb8179b51fbffc8dbb42d48d09b28ade5a07841e0bfbb6
+size 2791
---
+++ front-end/src/components/Sidebar.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:497681c5fe40e608aed304521c9f7fa88e52e9f076685360f2cec58555434f06
+size 2794
