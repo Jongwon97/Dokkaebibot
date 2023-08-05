5daf44d3f023ddbe36b0dac0f245b6ec74e5bd22
조원주 <dnjswn9178@hanmail.net>
2023-08-05 17:07:59 +0900

    fix(studyroom): edit input file type
    Former-commit-id: e9a2ead2703765ab7d138bfe78b047d13eb66c9b

--- front-end/src/pages/community/StudyroomCreate.tsx
+++ front-end/src/pages/community/StudyroomCreate.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f805a938523d5f48495b1b0d7207603d5af4dadde8a0caa336216bd8ba2f62d6
-size 5200
+oid sha256:f99b90f4ea161b8a81d9e796b59955f9c169af963dd434dc0e1144c18f28ba82
+size 5065
