b3d23feb136c8e97051c5f5a459fb0414a76c8cc
조원주 <dnjswn9178@hanmail.net>
2023-08-05 10:56:47 +0900

    style(mypage): find friend modal add styles
    Former-commit-id: fa8294930955b579f3b0ae9be2c6a7a29c212bf9

--- front-end/src/pages/members/comp/FriendFindModal.tsx
+++ front-end/src/pages/members/comp/FriendFindModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d3c7a1fd9aa503c09e122e409beca8b7f1a0d300d02a1ea6c42ffc054c6576d
-size 2491
+oid sha256:356422319e11dfd2c5ea91f770219b9d44a7c7ece90dbe162c25954907de9aa8
+size 2735
