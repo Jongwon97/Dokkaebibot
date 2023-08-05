9f535e5049a0a4a38fa57795e18dae9dfa8542c2
조원주 <dnjswn9178@hanmail.net>
2023-08-05 11:47:42 +0900

    fix(community): add useEffect dependency and goback func
    Former-commit-id: e1bf1ae5c427486f96adbc9405fb0c99a860c6af

--- front-end/src/pages/community/ArticleDetail.tsx
+++ front-end/src/pages/community/ArticleDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ceeb763c4876a85570cdaa1ecac4b26accebacd70253abd9d8f857b892caddd
-size 4866
+oid sha256:9a4daa968b260383ae68ba3b2581aab2abe6f58081e2830f490eaaff9898448e
+size 4958
