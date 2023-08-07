e88b0612369e43eb7033360d360d5b6bc0017cb9
홍의선 <hon3538@gmail.com>
2023-08-07 09:12:37 +0900

    feat(camera) : fixed
    Former-commit-id: 711657f20402ca433c843f815244844b2fae91e8

--- iot/dokkaebi/camera/camera.py
+++ iot/dokkaebi/camera/camera.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d2ff3fe3af491db5dce1a0683c759ac7775be502c6264dcee8d973e90135c17
-size 5189
+oid sha256:ecd4cf9d6d0a1480eacef64be49ac20bd859582514529afb64d8c05878cb33ba
+size 5188
