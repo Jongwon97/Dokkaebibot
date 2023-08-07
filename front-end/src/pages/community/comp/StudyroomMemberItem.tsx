ce82f4c98d59608275b1680b2008fdb4803ef646
조원주 <dnjswn9178@hanmail.net>
2023-08-07 17:50:48 +0900

    feat(study): add study page skeleton
    Former-commit-id: 71934d3b2ceb60c3f634c1ca5f7c39dbc8b3ff1b

--- front-end/src/pages/community/comp/StudyroomMemberItem.tsx
+++ front-end/src/pages/community/comp/StudyroomMemberItem.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f8e06670462f276d984b8967f979244e856cab1ab928a2e8b89274e2c97ac25
-size 1727
+oid sha256:114d4643de8a8663ab87d4c80d9a96fe9fed8b81dbc4cc99ee3ac842b6e6d386
+size 1843
