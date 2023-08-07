2c44fe1cc6a89e3bb40b88a20ce527adc5b91536
조원주 <dnjswn9178@hanmail.net>
2023-08-07 13:39:23 +0900

    fix(home): change home and connect page kkaegi img
    Former-commit-id: 9fcc93e1feecebac439e2b9dd805876c8c36c323

--- front-end/src/pages/home/Home.tsx
+++ front-end/src/pages/home/Home.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2bb7b56712df7bae63a669b8058ff614648b1bf04165878e4a445fa8c2e7393c
-size 1809
+oid sha256:2a5a2c8996fdc10604691957a08e1203e8de979f31eb76b4f8e50e064ea16e77
+size 1816
