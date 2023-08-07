06b0a83fdaa128ec812a814e102a2c6173a053cd
홍의선 <hon3538@gmail.com>
2023-08-07 15:17:28 +0900

    feat(camera): clean code for debug
    Former-commit-id: ad2e74faf905e963945a2afb6b4ad2924cea79d1

--- iot/dokkaebi/camera/camera.py
+++ iot/dokkaebi/camera/camera.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1174c6317cf76cf53789a5b9f6b9125b803ff604f39c4f022f19d0e26c5f9667
-size 5918
+oid sha256:a6b3cfd5b7cc6dc0a34df5680fbbf1254bded18608b61739f0629fc3ac79ee56
+size 8481
