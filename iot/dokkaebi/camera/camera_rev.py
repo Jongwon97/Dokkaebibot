bb5bf017d527fb63681f60080bc19202c7ad6522
홍의선 <hon3538@gmail.com>
2023-08-07 17:58:37 +0900

    feat(camera) : test
    Former-commit-id: 9d4b05b634bc95846844521f457fd7b0bcae1364

--- iot/dokkaebi/camera/camera_rev.py
+++ iot/dokkaebi/camera/camera_rev.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5aed139ea40cb5b2fe716ab4921cd5fedeecab6064318aa124d97e9b790941fe
-size 4405
+oid sha256:e890b04bde6f7779037a0464380d7294d51b851ee82ae1b02d03656b317420c9
+size 3810
