930adf38bf8512170c5012fcbe7ffd3c7fb53278
Byeongmin Ryoo <bmryu0501@gmail.com>
2023-08-07 17:54:42 +0900

    fix(main.py): removed invalid data
    Former-commit-id: 597b5a1f55fe5177216a8da11a32754bed7f75aa

--- iot/main.py
+++ iot/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f231a1c5b6b77a89f558dc1c0f8395e017ba9a8cdf3741d8aa3eaf6f3d4aa1b
-size 2849
+oid sha256:be174f78c32416cf15c146c9860464e4058591b1c86b86a77bd8b05f187e154b
+size 2414
