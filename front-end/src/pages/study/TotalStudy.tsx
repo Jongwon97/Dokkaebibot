f8decc71800d06bcd29413750f57a1ae88244b13
조원주 <dnjswn9178@hanmail.net>
2023-08-07 13:39:00 +0900

    feat(study): add skeleton code
    Former-commit-id: 2a22b49f269a5109d553a63e4cda800ce3798a7b

--- front-end/src/pages/study/TotalStudy.tsx
+++ front-end/src/pages/study/TotalStudy.tsx
@@ -1,11 +1,3 @@
-import React from 'react';
-
-function TotalStudy() {
-  return (
-    <div className="TotalStudy">
-      Total Study
-    </div>
-  );
-}
-
-export default TotalStudy;
+version https://git-lfs.github.com/spec/v1
+oid sha256:ab24f4e5eb0b869a73c6eb97d9930bb880a177a8791e3570989328238438f79a
+size 531
