7ea51ddf4789dc30f01f45e25ed26cd095677b14
조원주 <dnjswn9178@hanmail.net>
2023-08-08 14:43:05 +0900

    feat(members): add edit my profile setting fields
    Former-commit-id: 1116e95867396bcb52c76a6c1ab6c9401cf08efb

--- front-end/src/pages/members/MyPage.tsx
+++ front-end/src/pages/members/MyPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:756059f8a88cccf45abe7a1aef30019e00fe6bc0e5bcb9df78a4fc449d9c2cab
-size 2421
+oid sha256:c76ec9e0e97b27917c3bc217f8d016832c9ad776676953efe3150edc6edce060
+size 2255
