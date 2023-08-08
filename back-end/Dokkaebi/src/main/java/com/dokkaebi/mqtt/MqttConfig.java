35c24212657055e459879e8fffe9f0bd25cd4543
김동훈 <hunn000@gmail.com>
2023-08-08 17:16:45 +0900

    fix: use mqtt config
    Former-commit-id: c6ac8ecb77be4a01cbd464c35a5ef1a85d45cc08

--- back-end/Dokkaebi/src/main/java/com/dokkaebi/mqtt/MqttConfig.java
+++ back-end/Dokkaebi/src/main/java/com/dokkaebi/mqtt/MqttConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74749e08258af0e7609e1cc4ba89a8231fddf1ad7ba7b7c634cc1344c1eaaf79
-size 2418
+oid sha256:5d879f4875dae9a6cff9b1f67dc222b6f8deb24db5f8fb9a1593441d9cfbcc1d
+size 2286
