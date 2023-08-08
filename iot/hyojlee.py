b0998d5778924162e84fe301bd4ab36e2e7ce76c
euiseon <hon3538@gmail.com>
2023-08-08 17:59:29 +0900

    feat(iot/camera): add signal handler in main
    Former-commit-id: cd501dc32826afef1ac586e587f539de95581f51

--- iot/hyojlee.py
+++ iot/hyojlee.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48e3e206f39eda75699c9bf4aa06da27361439dc156bc6ef3e3a6f3ee569c728
-size 188
+oid sha256:0336e2c8f4f96e447e738ba093440099566addb26708ed90412df8d474df7d57
+size 383
