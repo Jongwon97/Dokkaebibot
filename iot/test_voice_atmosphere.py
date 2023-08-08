aecb82800de97d79b977b8295bf37b767800e2b9
Byeongmin Ryoo <bmryu0501@gmail.com>
2023-08-08 17:40:10 +0900

    fix(main.py): integrating codes
    Former-commit-id: 62838e99c28ce74c4eea41da6bcadbcff484a775

--- iot/test_voice_atmosphere.py
+++ iot/test_voice_atmosphere.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c738a0cfbb5020fe40c61b97d76061d00e3b318d55b27f88dc1a56e42f05097b
-size 2423
+oid sha256:30c70783bc84d5afc64fb4347419946fd504fa99270d3ade9b08ef1d238d2022
+size 2439
