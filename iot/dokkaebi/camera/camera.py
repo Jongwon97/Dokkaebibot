a80b8b6fd2f62737c77d260b999c172c6def84d1
이효정 <42.4.hyojlee@gmail.com>
2023-08-08 17:50:37 +0900

    feat(iot/camera): add weight in vscode
    Former-commit-id: 10919dfeeca322266ed3609dfb8dbb7ce578d710

--- iot/dokkaebi/camera/camera.py
+++ iot/dokkaebi/camera/camera.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:131498e7df7d4d47c5b5665c28d023168f42c080ae296cf038d212ab4fe7bb5d
-size 4399
+oid sha256:65580592c05a1304817c196cf8be23d5f056fffdb7cf04c844431906f86da5f0
+size 4521
