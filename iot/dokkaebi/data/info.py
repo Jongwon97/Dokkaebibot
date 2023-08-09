8ba573f5c05a809999a56f941d211e5edfa91937
Byeongmin Ryoo <bmryu0501@gmail.com>
2023-08-09 15:52:50 +0900

    fix(iot): fix typo
    Former-commit-id: c4f8a5613459d65ed1173d56475838d41f660dba

--- iot/dokkaebi/data/info.py
+++ iot/dokkaebi/data/info.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e288de8d3b70725f2165e8e94255e690047f70b4c7650d0c9ed16ccc2f65ef5
-size 407
+oid sha256:0701e42b53eff3be5bd53154be99b5fd11d9f14942fe9840b38cf70a23887b9d
+size 406
