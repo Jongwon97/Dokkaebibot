40b662617050b2a8620bafc194cd3acb5d53fe57
조원주 <dnjswn9178@hanmail.net>
2023-08-09 14:50:56 +0900

    feat(studyroom): add search studyroom by title
    Former-commit-id: b3a324c5f6ca14d1ffce11698a30260193e4773a

--- front-end/src/pages/community/StudyroomTotal.tsx
+++ front-end/src/pages/community/StudyroomTotal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88921ccb4f47e49cfb3932184ec07ebb10a98ea26f3ed5ab45f1f1976c971b86
-size 1960
+oid sha256:420c50137e91f6ed5bc7eb0fde7a4e7b3399c2597f0dab83f4b4d2c154065b46
+size 1819
