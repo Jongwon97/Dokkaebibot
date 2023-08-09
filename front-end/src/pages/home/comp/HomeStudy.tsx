e168cf5082517a64e55bcddd6e8b521cb3e88d08
조원주 <dnjswn9178@hanmail.net>
2023-08-09 17:52:42 +0900

    feat(home): add home study components
    Former-commit-id: 2a05a86e7a10bdd5b9906f4ba03ae61a8899cf80

--- front-end/src/pages/home/comp/HomeStudy.tsx
+++ front-end/src/pages/home/comp/HomeStudy.tsx
@@ -1,11 +1,3 @@
-import React from 'react';
-
-function HomeStudy() {
-  return (
-    <div className="HomeStudy">
-      Home Study
-    </div>
-  );
-}
-
-export default HomeStudy;
+version https://git-lfs.github.com/spec/v1
+oid sha256:4eef32b30d4642b7c231c16f49883a4028680b33515aa411a38cbc1fba0d6667
+size 1182
