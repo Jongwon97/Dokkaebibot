dca5e01a91ec48d9b5fc5c8d55bc68e60cf06067
조원주 <dnjswn9178@hanmail.net>
2023-08-09 15:45:47 +0900

    feat(studyroom): exit room button and confirm
    Former-commit-id: 05f9a8e77bb1f07a71233a16c66884dead8b1c55

--- front-end/src/pages/community/comp/StudyExitSetting.tsx
+++ front-end/src/pages/community/comp/StudyExitSetting.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45f7cc8f951be4f56ad385f3fdff0f26cb57db003a7a64d5d05e4b229dea15bc
-size 1585
+oid sha256:0eb097e768c86c1d55bb7407e4c970c04675d7982f7af220854cb7778de9b6f3
+size 3462
