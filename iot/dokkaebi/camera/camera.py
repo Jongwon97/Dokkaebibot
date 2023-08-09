e193b0d5eb31e3aa8a0739e7b35a024082e1eb41
이효정 <42.4.hyojlee@gmail.com>
2023-08-09 15:04:55 +0900

    fix(iot/camera): mqtt format in vscode
    Former-commit-id: e831a8933e6fa0b03f708c916e15fd3024b23c34

--- iot/dokkaebi/camera/camera.py
+++ iot/dokkaebi/camera/camera.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3e5e2f55b2bf9d993e733d6b3c2b1aea7ef173b7e5f073ea4452dca03a33a80
-size 4856
+oid sha256:b8d67eee55ace7afbded5f6cfc1b0594893893b2fa90fcccedf2ef909bc60d5c
+size 5069
