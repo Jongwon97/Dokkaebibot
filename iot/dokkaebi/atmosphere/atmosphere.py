dfbcc9576a790c9f7d8b012dfb2dd5c2e52b1bb2
Byeongmin Ryoo <bmryu0501@gmail.com>
2023-08-09 16:05:33 +0900

    fix(atmosphere.py): if data is None, send empty string
    Former-commit-id: 3e107a6d6afbcd1850f1c16e87c18ab2f6cf1dea

--- iot/dokkaebi/atmosphere/atmosphere.py
+++ iot/dokkaebi/atmosphere/atmosphere.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8988b942809b18f6a0d22a08114c20de4a7ac48c65b3a7cff3a1f53f8399aaa9
-size 1425
+oid sha256:0c29b5525ae8f9f63d8c94ef50961696cb12904d1ecead52f4f6af5651ec4954
+size 1646
