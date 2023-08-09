5d204154deedfde9ae3e6677c740085324b8b4d7
김동훈 <hunn000@gmail.com>
2023-08-09 15:37:27 +0900

    fix(WebSocketConfig): change client library version
    Former-commit-id: 8553536b3e8324852c573306a2bf14cbca8996e4

--- back-end/Dokkaebi/src/main/java/com/dokkaebi/config/WebSocketConfig.java
+++ back-end/Dokkaebi/src/main/java/com/dokkaebi/config/WebSocketConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15602e2576cfaac4a46fa238ddd1bdb2481414ae56dc9746701fa6b1aed93355
-size 1259
+oid sha256:d9a6b44089df29d8f566cc9a22eb1c9629e756bf7ab6cfb07456656cdafa298e
+size 1339
