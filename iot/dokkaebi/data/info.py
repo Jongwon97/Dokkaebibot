b32578fff51f107123610c0b6fe41753d976569c
이효정 <42.4.hyojlee@gmail.com>
2023-08-09 16:08:35 +0900

    fix(iot): mqtt in vscode
    Former-commit-id: f9f35b67fb49dfb2ff9f0a45d2848e2dfc207a98

--- iot/dokkaebi/data/info.py
+++ iot/dokkaebi/data/info.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7145a9bb7f664f90977f76f5cb2d8288f5e87088f722ba42bbaa328283821907
-size 366
+oid sha256:6e288de8d3b70725f2165e8e94255e690047f70b4c7650d0c9ed16ccc2f65ef5
+size 407
--- iot/main.py
+++ iot/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf7f45e109c968d8868c574f17cd5c3c4aebbc0023c5cd4d583265dca5b31dc3
-size 2665
+oid sha256:2b37804310aaa7e9ee11d1e4a667468e552f0d7f53e2d578ea134b24f849ce69
+size 2671
