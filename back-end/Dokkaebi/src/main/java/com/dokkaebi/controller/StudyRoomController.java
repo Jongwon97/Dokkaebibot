e30b0ade14463ea73f3ebb0d00fba6f0313bcea9
이종원 <whddnjs3214@naver.com>
2023-08-10 15:36:15 +0900

    feat: studyroom/delete
    Former-commit-id: 65ee6faf5d463fd9af68bcc6ec1c66ab2baa63da

--- back-end/Dokkaebi/src/main/java/com/dokkaebi/controller/StudyRoomController.java
+++ back-end/Dokkaebi/src/main/java/com/dokkaebi/controller/StudyRoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2317daf73d798d0673c27aea67f977ba56607f1fa4698c67fe97826cc16c2130
-size 4917
+oid sha256:6270b45e1a7e386fc38d88c409e3d549ef2e81e42e6ca3951908555464dea057
+size 4885
