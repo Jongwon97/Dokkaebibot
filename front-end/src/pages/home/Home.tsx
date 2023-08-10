a9ac94fc64e3658160bb28ff7ffe743a6d4cddf8
조원주 <dnjswn9178@hanmail.net>
2023-08-10 15:30:37 +0900

    fix(home): fix studyroom list error
    Former-commit-id: 6c0ec8e62a396bcdf907603d978453ee42cbf927

--- front-end/src/pages/home/Home.tsx
+++ front-end/src/pages/home/Home.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a63e8d334a200cb935e039873c8db0a19cbcdcd940ce27197f640ed34842e823
-size 2063
+oid sha256:7f9d81001cd423fea115ecf446a3914cddc80d3824da5f48baa8147ba550600f
+size 2076
