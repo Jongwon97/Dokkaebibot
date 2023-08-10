84e920f2a2186053b3eb4e61a799995bb3ffdb1a
조원주 <dnjswn9178@hanmail.net>
2023-08-10 17:52:42 +0900

    fix: delete news
    Former-commit-id: 66bcda0fae674786cf518b7e246ac4d35aa51d25

--- front-end/src/client/naverNews.ts
+++ front-end/src/client/naverNews.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f08972e4a92773ffa14e6ed5172450a2c44214fcee0d74c3588b9765d2439180
-size 895
+oid sha256:f3a8b37a3c441e0e49ee7cbcf73b5fe8e0437691a6e9833ce48c54edf7c74b3a
+size 874
