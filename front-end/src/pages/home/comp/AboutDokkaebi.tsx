5c17078f043564878ce7f2cd1e3eabfd3183cbc4
조원주 <dnjswn9178@hanmail.net>
2023-08-10 17:52:18 +0900

    feat(home): add about dokkaebi
    Former-commit-id: 8d22054542164ba53e65e996b6269586fac6d401

---
+++ front-end/src/pages/home/comp/AboutDokkaebi.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3ef26a0fe3615d60d41d4495527f7bc45172a417ec125354a0819bba2a7997b7
+size 1043
--- front-end/src/pages/home/comp/HomeStudy.tsx
+++ front-end/src/pages/home/comp/HomeStudy.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0c0fef1fe9994e58616947c6fe32a2fa7a24ce5a1b514b2439fe5dcd1637d5e
-size 1357
+oid sha256:66ee327b09c6eaf525050f4532e30d8b5c2458e738bfb37989f4c94adfb66825
+size 1317
