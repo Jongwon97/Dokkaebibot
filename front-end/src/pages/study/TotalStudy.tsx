98ebbf9850f6589b9df7065f13d8a09aeb2b31f4
김동훈 <hunn000@gmail.com>
2023-08-11 13:04:09 +0900

    fix: change dependencies
    Former-commit-id: e00d1b8d228f327fc47049bd5ec5e7d305952dca

--- front-end/src/pages/study/TotalStudy.tsx
+++ front-end/src/pages/study/TotalStudy.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3cee1349201af742a4462870f9e3be94a05b9a727eb5b1655754bae1bfa97fbc
-size 1287
+oid sha256:d19b4a8451db17830444be2c2b65e5753e92074211f15a207f9828c75ed9d9d6
+size 1216
