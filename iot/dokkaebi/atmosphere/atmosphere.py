b4a725ee0680a9a71d389368b035ee9a8b6ef571
42.4.hyojlee <42.4.hyojlee@gmail.com>
2023-08-11 13:56:04 +0900

    feat(iot/master): camera.py 130까지 찍힘
    Former-commit-id: cab1f5915295c7800635bbf6ff0088738005be2f

--- iot/dokkaebi/atmosphere/atmosphere.py
+++ iot/dokkaebi/atmosphere/atmosphere.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe3716119c2ec4b45901a08f8fbf93cf4720954c59ee0c75c8b74fc3f076e602
-size 1651
+oid sha256:25248553b247cc9b90134c84c845e7ce88ff38a8409bb43090f58972bed7bc07
+size 1704
--- iot/dokkaebi/camera/camera.py
+++ iot/dokkaebi/camera/camera.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7290c949336fe383d9521958edbde645d77feb33a82351816ebda7a43ef9027
-size 5281
+oid sha256:71617dd66b356fb5b31e2c319ca196eedef251cdc999404c915c418035b5077b
+size 5482
