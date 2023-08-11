6a7fa0b9999d15006e4824852438baaffdaba5c8
조원주 <dnjswn9178@hanmail.net>
2023-08-11 14:53:31 +0900

    fix(home): change img about 01
    Former-commit-id: 9f43b7962666c92d4de281d6a4ae88c30eeb3cad

--- front-end/src/client/analysis.ts
+++ front-end/src/client/analysis.ts
@@ -1,3 +1,11 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e01e1f33b5678faf8ec6f9d16ced6f4f09526f95e63a30f4db7a723d1c906d28
-size 279
+import { client, clientWithToken } from "./client";
+
+const analysisURL = 'analysis/'
+
+export const getGraphData = (start:string, end:string) => {
+	console.log("aaaaaa")
+	console.log(start)
+	console.log(end)
+    return client().get(analysisURL +  start + '/' + end + '/graph')
+}
+
