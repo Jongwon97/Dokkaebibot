08b20cbe9589bff2a9162879fc11e791760e97f7
김동훈 <hunn000@gmail.com>
2023-08-14 01:21:23 +0900

    fix(pages): change filename notice to Notice
    Former-commit-id: 2c930a8d7c05d661f3dad3bfaccc64c3a8648009

--- front-end/src/pages/members/LogIn.tsx
+++ front-end/src/pages/members/LogIn.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:954e0e6b2d39adf34add6ec7256ab0fc38d24f19c81a48857be1a608135abfb9
-size 3651
+oid sha256:28761b76aa5113be7a27042950b0f632a98d56a6162266f23d1144dcf362aca2
+size 3628
