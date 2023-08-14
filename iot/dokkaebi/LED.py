c51a18ca3a0340a659ed5398505f5815bdeaeb30
42.4.hyojlee <42.4.hyojlee@gmail.com>
2023-08-14 15:43:34 +0900

    refactor(iot/LED): done LED
    Former-commit-id: 7f2eb168077e0c352f2e28bd03e1310ba129cdb0

--- iot/dokkaebi/LED.py
+++ iot/dokkaebi/LED.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed9a28c9ab8fa61ea3a0f6d7b9993adcd19a860ba03267a0dc4f107984b1977c
-size 1832
+oid sha256:fa2625adbc70b2181782ef3b6d99c7e9894b54548dbba8020b6da1d48df192ec
+size 1786
--- iot/dokkaebi/voice/voice.py
+++ iot/dokkaebi/voice/voice.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:187b00eafa2773641274f4866b085f0dab18ee6ea021c916f69745fdd92beedd
-size 2736
+oid sha256:68e87794289fbfff410536c0b42d6a7882e058858c0b68a39b0bf72483e4ee56
+size 2445
