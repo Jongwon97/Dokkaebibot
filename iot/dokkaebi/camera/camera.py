cd08c2031e531426dc9c03fa44d45d605a170a52
42.4.hyojlee <42.4.hyojlee@gmail.com>
2023-08-14 16:31:39 +0900

    feat(iot/voice): add sleep tts
    Former-commit-id: 0f1c0875fbd6a7802fc6f2bb5e85c11eedbf6c95

--- iot/dokkaebi/camera/camera.py
+++ iot/dokkaebi/camera/camera.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:600c6b9cae772f87f23b2afe035781900266f5cfc015cd1bfb7ffe35bb57524c
-size 5599
+oid sha256:3f7565fe6cf539f7bf98135cba857c69a4278f004c564f739b51c496f8ce1249
+size 5340
---
+++ iot/dokkaebi/camera/tts.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8478e6b4c96a64b785d1acaeec8e019af6d40cad3e1b18552b9af3721559b5f9
+size 373
