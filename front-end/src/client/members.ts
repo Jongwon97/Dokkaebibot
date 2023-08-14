a0df98f3525368cf8e90bfaaec4915f16012c638
조원주 <dnjswn9178@hanmail.net>
2023-08-14 09:39:20 +0900

    feat(members): add study hard and bad pose img
    Former-commit-id: 7446ffe970c7be6fa46d84c53d63b1397f4e12e4

--- front-end/src/client/members.ts
+++ front-end/src/client/members.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b82d2aa3bf48ac51a6d554c9845a75a7c320f8a8ee19680091c8487e735ac28a
-size 1902
+oid sha256:bacc6dcae8133561774e639f8ead3e0f80699dd5f5b71f0d1c072237541b3e85
+size 2036
--- front-end/src/pages/study/comp/StudyState.tsx
+++ front-end/src/pages/study/comp/StudyState.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8da536a95dd3e3c73303199a44ac35137d7114b9befa75f62e107c2d3b71752d
-size 2906
+oid sha256:17a9e3513bb5e31ef96b8e97520deabf14c0d5c2074263b54cd3a30595959f6e
+size 3137
