9dcdab893f4ae4cc1d9e8be465c20613274bb34d
42.4.hyojlee <42.4.hyojlee@gmail.com>
2023-08-14 15:33:14 +0900

    feat(iot/LED): Ongoing LED
    Former-commit-id: 0ce981678b9fe1c3f31af886c5b3d38a7b2c00cc

--- iot/dokkaebi/LED.py
+++ iot/dokkaebi/LED.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2b7cda3dd606bf462cb5f832cb77a39f2fcba4f0badacefff7e3f277684497a
-size 1684
+oid sha256:fe1d4a677a52af9857234c713843e843bfb2a4a607aa4174f6b8118298f60c49
+size 1696
--- iot/dokkaebi/voice/voice.py
+++ iot/dokkaebi/voice/voice.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15c28819875934b1fff178899daba44c67bca9922b6d3ea195b16bcca06fe06f
-size 2740
+oid sha256:187b00eafa2773641274f4866b085f0dab18ee6ea021c916f69745fdd92beedd
+size 2736
