023cd02e045d7bf54d7ddaab4bcdee6bc13ab854
김동훈 <hunn000@gmail.com>
2023-08-14 17:14:15 +0900

    fix: change error message at signup password confirm
    Former-commit-id: 0ee334e41f8e68fc62446eee58f4fba0bc3212f2

--- front-end/src/pages/members/SignUp.tsx
+++ front-end/src/pages/members/SignUp.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b385338ae16d74b1af11d3bbce0eb80a474b1cbfd99482f3dcf443338f4b092a
-size 4338
+oid sha256:cbe3684ebe5b9e762b9063271706c74fc3bdf0c7ddd070908f997fb9664f9199
+size 4413
