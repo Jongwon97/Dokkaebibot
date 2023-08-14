a7cde03e205ac42fc10f656b095fd5f7a88bfccd
이효정 <42.4.hyojlee@gmail.com>
2023-08-14 10:41:08 +0900

    test(iot/LED): test singleton in vscode
    Former-commit-id: e63486498985fbe721e1d49173401350394035aa

--- iot/dokkaebi/LED.py
+++ iot/dokkaebi/LED.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:482b0f0b104bbcf6eeede39d249bbf7259b19946739d308c20a4ed1de1bcc0d8
-size 1465
+oid sha256:2be796394884380a7d5d88c322f82c458e5781b6db0adda1c193812186caf6ab
+size 1438
