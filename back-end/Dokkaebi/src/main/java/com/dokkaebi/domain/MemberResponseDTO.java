2df679f692c33a4095cd5c503052129b22524c11
조원주 <dnjswn9178@hanmail.net>
2023-08-14 17:09:57 +0900

    style(member): fix align and error messages
    Former-commit-id: 6791e7f3d1e9bd8133e14148df4d54b057fbdd12

--- back-end/Dokkaebi/src/main/java/com/dokkaebi/domain/MemberResponseDTO.java
+++ back-end/Dokkaebi/src/main/java/com/dokkaebi/domain/MemberResponseDTO.java
@@ -1,20 +1,3 @@
-package com.dokkaebi.domain;
-
-import lombok.Data;
-
-@Data
-public class MemberResponseDTO {
-  private Long id;
-  private String email;
-  private String nickname;
-  private Integer iconNumber;
-  private Boolean haveDevice;
-  
-  public MemberResponseDTO(Member member) {
-    id = member.getId();
-    email = member.getEmail();
-    nickname = member.getNickname();
-    iconNumber = member.getIconNumber();
-    haveDevice = (member.getDevice() != null);
-  }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:5f7549556c7f553b8486fe5dbf7e9386851c95b4ef2fb735fe08235ae39647b6
+size 454
--- front-end/src/pages/members/comp/MyProfileSetting.tsx
+++ front-end/src/pages/members/comp/MyProfileSetting.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4921734b72409d4470e92ef55daadd1bd01eb096bbac38bbf03ddc8d575ca25f
-size 8608
+oid sha256:5cc2bd30180cd05ff5bb8e9efd8162720ace11a07001f9169c00636d1609323d
+size 8790
