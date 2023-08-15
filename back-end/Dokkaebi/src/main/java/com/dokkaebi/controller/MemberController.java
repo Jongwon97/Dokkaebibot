2579db768920610f53f6c86836d8918d8596d7ae
이종원 <whddnjs3214@naver.com>
2023-08-15 16:56:28 +0900

    feat: zandi complete
    Former-commit-id: d85c5ba52796f15a485117c65e78ededaa049197

--- back-end/Dokkaebi/src/main/java/com/dokkaebi/controller/MemberController.java
+++ back-end/Dokkaebi/src/main/java/com/dokkaebi/controller/MemberController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5e6e50aff13d725a51bdb8fbead7542aa6677d436fff106696518696ba22028
-size 8288
+oid sha256:180f4fdd04eee3bdfa3a4d618a9ee8ab2268accf0b86dabdc37ea1c592d991ed
+size 8422
