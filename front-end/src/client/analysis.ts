d1291e3b1c495ae859596931ae9d12af769a2f20
조원주 <dnjswn9178@hanmail.net>
2023-08-16 14:57:33 +0900

    style: studyroom list align
    Former-commit-id: 16e31865503a242d5c8a6ac08664cd40443e7e92

--- front-end/src/client/analysis.ts
+++ front-end/src/client/analysis.ts
@@ -1,3 +1,17 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:11f3152889d9f44318331fb574a6616efe1939489f6bca65037036095b110d9f
-size 410
+import { client, clientWithToken } from "./client";
+
+export interface Ranker {
+    nickname: string,
+    iconNumber: number,
+    timeSum: string
+}
+
+const analysisURL = 'analysis/'
+
+export const getGraphData = (start:string, end:string) => {
+    return clientWithToken().get(analysisURL +  start + '/' + end + '/graph/check')
+}
+
+export const getRanker = () => {
+    return client().get(analysisURL + 'ranker')
+}
--- front-end/src/pages/members/comp/FriendSetting.tsx
+++ front-end/src/pages/members/comp/FriendSetting.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa7f8fd72de14d25fddf2d0d9e77b72cd0b3f0a4c60f16d7ab8941d4195355ee
-size 4961
+oid sha256:2b1c806c6f0e4e612fe995496dc011ac60573dba56509fcf93b806a1efa2ad2b
+size 4726
