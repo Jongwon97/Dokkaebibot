b3cf5b61a3d524b23318992ac419df84e4103670
김동훈 <hunn000@gmail.com>
2023-08-16 15:08:54 +0900

    fix: add delete button only to comment writer
    Former-commit-id: c98ebea18320bfb5a237a7b2112308ff24d44103

--- front-end/src/pages/community/comp/ArticleComment.tsx
+++ front-end/src/pages/community/comp/ArticleComment.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d5cafaeb4102efc027eacdb3daac55e6a38ffbf80add7e5a01d2754e74ea11e
-size 3579
+oid sha256:65fb5b79173896dfed7e3c76fcbfbf845c9dd94fc163f5ba416c8469ae7c980c
+size 3573
