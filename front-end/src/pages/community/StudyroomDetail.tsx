14e0f8cf89dbb19fd5c3dd9a836e9400b139ebae
이종원 <whddnjs3214@naver.com>
2023-08-17 00:54:25 +0900

    feat: timeSet
    Former-commit-id: 4a91b337d3729d31084885b0000f7b5401dc7a18

--- front-end/src/pages/community/StudyroomDetail.tsx
+++ front-end/src/pages/community/StudyroomDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43e3c0f109a42c2bc2dcbbf49932d0524f74e19bdd0a72c7f42c590c8d97e2b3
-size 8255
+oid sha256:386b3486b9de76d25a9851d02c30f3a62626dd6ec4a52b4dfb69c5176e77a0d9
+size 8338
--- front-end/src/pages/community/comp/StudyroomMemberItem.tsx
+++ front-end/src/pages/community/comp/StudyroomMemberItem.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:080e32b8b4503cca2b7bfeda60459b71323a8ef95d47c6fccc059bea2aaeb7f3
-size 3318
+oid sha256:e77be2af17ebd71957d3ff062e3c39daef406b9f6144d9dbcca2ffed28cec105
+size 3319
