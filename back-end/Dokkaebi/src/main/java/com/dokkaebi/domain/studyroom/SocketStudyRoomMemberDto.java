1f4f31ba4685e2d638577e54973660458240c02c
이종원 <whddnjs3214@naver.com>
2023-08-16 10:41:15 +0900

    feat: icon
    Former-commit-id: 26cb2d20e3a943c55c5ba648b1345f115697b612

--- back-end/Dokkaebi/src/main/java/com/dokkaebi/domain/studyroom/SocketStudyRoomMemberDto.java
+++ back-end/Dokkaebi/src/main/java/com/dokkaebi/domain/studyroom/SocketStudyRoomMemberDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7765157ba5a3aacd233900b210c5bf1ec6db74d214bef882513d8a6495a38dd9
-size 803
+oid sha256:4d629680d1394b823105f78d0947e9a3cbec02943989266cba05a3ea854840bc
+size 837
