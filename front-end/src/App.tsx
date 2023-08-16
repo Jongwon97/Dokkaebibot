42a3324223cacb6f8d8e3145593bb68bd9153c7f
조원주 <dnjswn9178@hanmail.net>
2023-08-16 13:23:06 +0900

    feat(route): add private routes redirect to login
    Former-commit-id: ff310857b6f4c3895dc5c8483fa797ee5b35f94f

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bbe2d4653b5994eac3c76ce98b3be77163f4e5a1bf5e1227a4e71eebde2c0695
-size 3107
+oid sha256:52ed240f3603810c2e46e61bea970a20ba70b8a84e55f4cf7d6abdbfdecf7bba
+size 2858
