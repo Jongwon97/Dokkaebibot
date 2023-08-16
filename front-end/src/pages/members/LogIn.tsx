4de589e5f586405fe05b0684aa3afc596f6d28d3
김동훈 <hunn000@gmail.com>
2023-08-16 09:19:25 +0900

    fix: rank with no data
    Former-commit-id: 1048e4c0030cedc076a6fb16a04b57bc1998f736

--- front-end/src/pages/members/LogIn.tsx
+++ front-end/src/pages/members/LogIn.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e91c0379224731b1ed048f4e9c6d6ec3dc5ce1363daf428631ffe223cfb1e46
-size 3655
+oid sha256:97bd5c4aedf851c93439f19ff9454da63f2c8261a37e721f8f5cfb1d3d3a2d44
+size 3710
