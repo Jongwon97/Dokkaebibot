df452dc4482fb453a9875e1df5aad65ae1183517
김동훈 <hunn000@gmail.com>
2023-08-16 17:33:10 +0900

    fix: change double parsing
    Former-commit-id: ff7c0dda634cf0e536189aefe97ce4b575d4270a

--- back-end/Dokkaebi/src/main/java/com/dokkaebi/mqtt/MqttRepository.java
+++ back-end/Dokkaebi/src/main/java/com/dokkaebi/mqtt/MqttRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15b87b0d15f911c74448a192d3c922d6977d501ec267cf6087633937c22b1821
-size 1015
+oid sha256:59fcc532ff0f0f27cb21efdb154de04fefdf047f63ed1d977cd86a9289b16b5f
+size 1132
