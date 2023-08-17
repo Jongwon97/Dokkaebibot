d05651cc18386e03768d5345c6b91080176ab36a
조원주 <dnjswn9178@hanmail.net>
2023-08-17 14:10:23 +0900

    fix(studyroom): fix search studyroom code
    Former-commit-id: fd0dca86dfabde5721008e48cf6a1551111ef848

--- front-end/src/pages/community/comp/StudyroomListItem.tsx
+++ front-end/src/pages/community/comp/StudyroomListItem.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2691a4589779bf40dfcd1be4dbca85877effad43cb41026c3507d750f5fc451
-size 5151
+oid sha256:67412f477b3fe7fee367f105912954b38cc4b6243f2e735f9a32a5a17da3754d
+size 5304
