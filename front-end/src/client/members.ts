2c451c2f5b2f6cbcac68799666b97258e161fd52
조원주 <dnjswn9178@hanmail.net>
2023-08-17 10:58:36 +0900

    feat(members): add ssafy icon
    Former-commit-id: b389455d81d5dd0fac44438175cd3a81923ffe10

--- front-end/src/client/members.ts
+++ front-end/src/client/members.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66ea707a84a3158b788b8b4a5e495722ebe61047fbda643e7b01763d852f6bee
-size 2339
+oid sha256:79e7c7026215b68efb5a5257f86827bdf25c16fe461b5b2a9eb0e642f6958260
+size 2720
