8ec87c93d1f15d6fec4fc58225c4e59f802faf4e
김동훈 <hunn000@gmail.com>
2023-08-18 11:31:02 +0900

    Update back-end/Dokkaebi/src/main/java/com/dokkaebi/service/StudyDataService.java
    Former-commit-id: 8fa376016a12417408406d46fddece6b2c82c4e2

--- back-end/Dokkaebi/src/main/java/com/dokkaebi/service/StudyDataService.java
+++ back-end/Dokkaebi/src/main/java/com/dokkaebi/service/StudyDataService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae42dcafe3262125abb86f07fd98e35ef4c0f9865c187a532ae5b9d830707919
-size 7226
+oid sha256:5530dc40c8db3ea41efbfa8e393578a3c584d4516142bd2c29480d4e8d1a9dbd
+size 7681
